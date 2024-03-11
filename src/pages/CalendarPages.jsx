6e66643895683f3b8f779d1d9ae575032503d12b
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-11 23:35:01 +0100

    Created a new function that fetches the todo of a particular day from the local storage. It fetches everything but filters it to that particular day

--- src/pages/CalendarPages.jsx
+++ src/pages/CalendarPages.jsx
@@ -5,18 +5,29 @@ import NavContext from "../context/NavContext";
 
 function CalendarPages() {
   // const [date, setDate] = useState(new Date())
-  const {shownav, date, setDate}= useContext(NavContext)
+  const {shownav, date, setDate, setTodos}= useContext(NavContext)
 
-  // const onCh = (value)=>{
-  //   setDate(value)
-  //   // console.log(date.toDateString());
-  // }
+  const onCh = (value)=>{
+    setDate(value)
+
+
+    
+    const retrieveItem = JSON.parse(localStorage.getItem('todos'))
+
+    
+    console.log(retrieveItem.filter((item)=> item.dateString === value.toDateString()));
+    // setTimeout(() => {
+
+      setTodos(retrieveItem.filter((item)=> item.dateString === value.toDateString()))
+    // }, 1000);
+    // console.log(date.toDateString());
+  }
   return (
     <div className={`${shownav?'hidden':'block'}`}>
       {/* <button onClick={()=>{setShownav(!shownav)}} className="">
         Click me
       </button> */}
-        <Calendar value={date} onChange={(value)=>setDate(value)} className=''/>
+        <Calendar value={date} onChange={(value)=>onCh(value)} className=''/>
 
         <p className=" text-3xl">
           {
