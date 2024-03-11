7da1cb49c43ed374788c283589bf432237e5a05d
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-11 23:32:45 +0100

    Created the task page, it changes based on the date selected. Displays the todo options for that day. Still functionality nothing done yet on design

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,10 +1,45 @@
+import { useContext, useEffect } from "react"
+import NavContext from "../context/NavContext"
+import TaskItem from "../component/TaskItem"
 
 function Tasks() {
+  const {todos,date, setTodos} = useContext(NavContext)
+  
+  useEffect(()=>{
+    const retrieveItem = JSON.parse(localStorage.getItem('todos'))
+    setTodos(retrieveItem.filter((item)=> item.dateString === date.toDateString()))
+  },[])
+  if(todos.length < 1){
+    return <p>
+      No Tasks for Today
+    </p>
+  }
   return (
     <div>
         <h1 className=" font-semibold text-2xl ">
             Tasks List
         </h1>
+
+        {/* <p>
+          {
+            todos.details
+          }
+        </p> */}
+        <p>
+          {
+            date.toDateString()
+          }
+        </p>
+
+        {
+          todos.length >= 1? 
+          todos.map((todo,index)=>(
+            <TaskItem todo={todo} key={index}/>
+          )) :
+          <p>
+            Nothing to load mofo
+          </p>
+        }
     </div>
   )
 }
