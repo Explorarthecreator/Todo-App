99a18861e163140d27d53f40c8cf8a4248f676e5
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-12 13:21:24 +0100

    Created an empty todo component that will show in the case of no todo for a particular

---
+++ src/component/EmptyTodo.jsx
@@ -0,0 +1,12 @@
+
+function EmptyTodo() {
+  return (
+    <div>
+        <p>
+            No tasks for today!
+        </p>
+    </div>
+  )
+}
+
+export default EmptyTodo
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,41 +1,21 @@
 import { useContext, useEffect } from "react"
 import NavContext from "../context/NavContext"
 import TaskItem from "../component/TaskItem"
+import EmptyTodo from "../component/EmptyTodo"
 
 function Tasks() {
-  const {todos,date} = useContext(NavContext)
-  
-  
-  if(todos.length < 1){
-    return <p>
-      No Tasks for Today
-    </p>
-  }
+  const {todos} = useContext(NavContext)
   return (
     <div>
         <h1 className=" font-semibold text-2xl ">
-            Tasks List
+            Tasks
         </h1>
-
-        {/* <p>
-          {
-            todos.details
-          }
-        </p> */}
-        <p>
-          {
-            date.toDateString()
-          }
-        </p>
-
         {
           todos.length >= 1? 
           todos.map((todo,index)=>(
             <TaskItem todo={todo} key={index}/>
           )) :
-          <p>
-            Nothing to load mofo
-          </p>
+          <EmptyTodo/>
         }
     </div>
   )
