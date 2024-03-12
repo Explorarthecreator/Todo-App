05afad2fb5c660518b2cfd9cbbedeaeb66d77f42
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-12 22:50:34 +0100

    removed that warning from react when using use effect

--- src/context/NavContext.js
+++ src/context/NavContext.js
@@ -3,16 +3,16 @@ import { createContext, useState, useEffect } from "react";
 const NavContext = createContext()
 
 export const NavbarProvider = ({children})=>{
-    const [shownav, setShownav] = useState(false)
+    const [shownav, setShownav] = useState(true)
     const [date, setDate] = useState(new Date())
     const [todos, setTodos] = useState([])
 
-    // eslint-disable-next-line
     useEffect(()=>{
         const retrieveItem = JSON.parse(localStorage.getItem('todos'))
         if(retrieveItem){
             setTodos(retrieveItem.filter((item)=> item.dateString === date.toDateString()))
         }
+    // eslint-disable-next-line
     },[])
     return <NavContext.Provider value={{
         shownav,
