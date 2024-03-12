7baac612dafd13802809a0e11f06fa61501b5455
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-12 16:50:59 +0100

    Styled the App.js file-basically the landing page- handling responsiveness too.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import NavBar from './component/NavBar';
 import CalendarPages from './pages/CalendarPages';
 import { NavbarProvider } from './context/NavContext';
 import Tasks from './pages/Tasks';
+import Greeting from './component/Greeting';
 
 function App() {
   return (
     <NavbarProvider>
-      <div>
+      <div className=' bg-[#fff] text-[#101828] h-screen overflow-y-scroll px-4 lg:px-0'>
         <NavBar/>
         {/* <B/> */}
-        <main>
-          <CalendarPages/>
-          <div>
+        <main className=' lg:w-4/5 lg:m-auto lg:my-6'>
+          <Greeting/>
+          <div className=' lg:flex lg:justify-between lg:mt-10'>
             <Tasks/>
+            <CalendarPages/>
           </div>
         </main>
       </div>
