5b0adb719231eaead23cb6ff651804d136c5a138
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-12 22:48:32 +0100

    Did some style change and setup the toast container for the react toastify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CalendarPages from './pages/CalendarPages';
 import { NavbarProvider } from './context/NavContext';
 import Tasks from './pages/Tasks';
 import Greeting from './component/Greeting';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -12,12 +14,23 @@ function App() {
         {/* <B/> */}
         <main className=' lg:w-4/5 lg:m-auto lg:my-6'>
           <Greeting/>
-          <div className=' lg:flex lg:flex-row-reverse lg:justify-between lg:mt-10'>
+          <div className=' lg:flex lg:flex-row-reverse lg:justify-between lg:mt-10 mb-5'>
             <CalendarPages/>
             <Tasks/>
           </div>
         </main>
       </div>
+      <ToastContainer position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={true}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
     </NavbarProvider>
   );
 }
