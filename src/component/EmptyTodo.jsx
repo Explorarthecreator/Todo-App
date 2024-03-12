c151d46ceb5d8dae4d8031dee332ea150a0f75b2
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-12 17:48:14 +0100

    Added an SVG that will be used in the Empty todo component

--- src/component/EmptyTodo.jsx
+++ src/component/EmptyTodo.jsx
@@ -1,8 +1,10 @@
+import AddTaskSVG from '../undraw_add_notes_re_ln36.svg'
 
 function EmptyTodo() {
   return (
-    <div>
-        <p>
+    <div className='text-center'>
+      <img src={AddTaskSVG} alt="" className=' w-4/5 lg:w-1/2 m-auto mt-20 lg:mt-7 mb-4'/>
+        <p className=' text-xl font-medium'>
             No tasks for today!
         </p>
     </div>
