72ff1e76b2ba09af386e05833dc57ec3305e57dd
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-15 16:24:37 +0100

    Added a greeting variable that greets according to the time of the day

--- src/component/Greeting.jsx
+++ src/component/Greeting.jsx
@@ -2,14 +2,14 @@ import { useContext } from "react"
 import NavContext from "../context/NavContext"
 
 function Greeting() {
-    const {todos} = useContext(NavContext)
+    const {todos, greetings} = useContext(NavContext)
   return (
     <div className=" my-4">
         <h1 className=" text-2xl lg:text-3xl font-semibold">
-            Good morning!
+            Good {greetings}!
         </h1>
         <p className={`${todos.length>=1?'block':'hidden'} text-[#475467] mt-1`}>
-          You got some task to do
+          You've got some task to do
         </p>
     </div>
   )
--- src/context/NavContext.js
+++ src/context/NavContext.js
@@ -6,11 +6,22 @@ export const NavbarProvider = ({children})=>{
     const [shownav, setShownav] = useState(false)
     const [date, setDate] = useState(new Date())
     const [todos, setTodos] = useState([])
+    const [greetings, setGreetings] = useState('Morning')
 
     useEffect(()=>{
         const retrieveItem = JSON.parse(localStorage.getItem('todos'))
         if(retrieveItem){
             setTodos(retrieveItem.filter((item)=> item.dateString === date.toDateString()))
+        }
+
+        if(date.getHours() < 12){
+            console.log('Good Morning')
+            setGreetings('Morning')
+        }else if (date.getHours() >= 12 && date.getHours() <=16){
+            setGreetings('Afternoon')
+        }
+        else{
+            setGreetings('Evening')
         }
     // eslint-disable-next-line
     },[])
@@ -18,6 +29,7 @@ export const NavbarProvider = ({children})=>{
         shownav,
         date,
         todos,
+        greetings,
         setTodos,
         setShownav,
         setDate
