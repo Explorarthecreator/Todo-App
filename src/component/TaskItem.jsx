15db47934788daaa9edf12d8715cfd7c967c07e9
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-15 16:23:56 +0100

    Made some style changes and added the icons for edit and delete todo. Function not yet added

--- src/component/TaskItem.jsx
+++ src/component/TaskItem.jsx
@@ -1,17 +1,25 @@
-
+import { FaPen, FaTrash } from "react-icons/fa"
 function TaskItem({todo}) {
   return (
-    <div className=" bg-[#EAECF0] w-11/12 m-auto mb-4 rounded-md p-4 shadow-lg">
-      <p className=" text-sm lg:text-base text-[#101828] font-medium mb-1">
-        {
-          todo.details
-        }
-      </p>
-      <p className=" text-xs text-[#475467]">
-        {
-          todo.dateString
-        }
-      </p>
+    <div className=" bg-[#EAECF0] w-11/12 m-auto mb-4 rounded-md p-4 shadow-lg flex justify-between items-center">
+      <div className=" w-4/5">
+        <p className=" text-sm lg:text-base text-[#101828] font-medium mb-1 w-11/12 lg:w-full">
+          {
+            todo.details
+          }
+        </p>
+        <p className=" text-xs text-[#475467]">
+          {
+            todo.dateString
+          }
+        </p>
+      </div>
+
+      <div className="flex gap-4">
+        <FaPen className=" cursor-pointer"/>
+        <FaTrash className="cursor-pointer"/>
+      </div>
+      
     </div>
   )
 }
