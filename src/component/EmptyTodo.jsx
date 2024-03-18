e494d7f13dc63453876331242fe91c785384217b
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-18 16:30:29 +0100

    Reduced the width of the image in the empty state

--- src/component/EmptyTodo.jsx
+++ src/component/EmptyTodo.jsx
@@ -3,7 +3,7 @@ import AddTaskSVG from '../undraw_add_notes_re_ln36.svg'
 function EmptyTodo() {
   return (
     <div className='text-center'>
-      <img src={AddTaskSVG} alt="" className=' w-4/5 lg:w-1/2 m-auto mt-20 lg:mt-7 mb-4'/>
+      <img src={AddTaskSVG} alt="" className=' w-4/5 lg:w-4/12 m-auto mt-20 lg:mt-7 mb-4'/>
         <p className=' text-xl font-medium'>
             No tasks for today!
         </p>
